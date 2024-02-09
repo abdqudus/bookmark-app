057cb09b49bb783bdf24e9a89d04ed9394d1fca7
unknown <abdkhudusadeberyo@gmail.com>
2024-02-09 19:48:35 +0100

    fixed spacing in folder name bug

--- src/utils/getParentFromPath.js
+++ src/utils/getParentFromPath.js
@@ -1,6 +1,6 @@
 export const getParentFromUrl = (url) => {
   const pathName = url.pathname;
   const pathArr = pathName.split("/");
-  const parent = pathArr[pathArr.length - 1].replace("%20", " ");
+  const parent = pathArr[pathArr.length - 1].replace(/%20/g, " ");
   return parent;
 };
